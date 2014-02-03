6cab5205a9e79f09090ace0b4b2a8f8e66094272
nlalex <nathan.l.alexander@gmail.com>
2014-02-03 17:38:32 -0500

    Removed all CO & Temp/Hum code

--- Arduino01.ino
+++ Arduino01.ino
@@ -10,43 +10,24 @@
 
 SoftwareSerial xbeeSerial = SoftwareSerial(rxPin, txPin);
 
-// Needed for humidity sensor
-#include <Wire.h> //I2C library
-float hum();
-float temp();
-#define TRUE 1
-#define FALSE 0
-
+// Pinout
 int pTMP = A0; 
-int pLDR = A1;
+int pLDR1 = A1;
+int pLDR2 = A2;
 int pPIR = A5;
-int pCOT = 12; // CO trigger
-int pCOS = A2; // CO sensor
+int pHUM = A6;
 
-long tCO = 0;
-long tCO_wait = 60000;
-
-int iCO_last = 0;
 
 float vPIR = 2000;
 float vTMP = 0;
 float vLDR = 0;
-float vCO = 0;
 float vHUM = 0;
-float vTH = 0;
 int n = 0;
 
 void setup()
 {
   Serial.begin(9600); 
   xbeeSerial.begin(9600);
-  pinMode(pCOT, OUTPUT); // For CO sensor
-  
-  // Following needed for humidity sensor
-  Wire.begin();
-  pinMode(4, OUTPUT);
-  digitalWrite(4, HIGH); // this turns on the HIH3610
-  delay(5000);
 }
  
  
@@ -133,93 +114,6 @@ int fLDR() {
   int reading = analogRead(pLDR);
   return reading;
 }
-
-//int fPIR() {
-//  // Returns PIR value from 0-1023
-//  int reading = analogRead(pPIR);
-//  return reading;
-//}
-
-//int fCO(int rLast) {
-//  // Turns CO sensor heating element on or off; takes reading if time allottment has passed
-//  // Returns stagnant value if not new
-//  int reading;
-//  if(millis() - tCO < tCO_wait) {
-//    analogWrite(pCOT, 255); // Needs to be at 5V for 60s
-//    reading = rLast;
-//  } else {
-//    analogWrite(pCOT, 67); // Low voltage 1.4V
-//    delay(1000);
-//    reading = analogRead(pCOS); 
-//    analogWrite(pCOT,255);
-//    tCO = millis();
-//  }
-//  return reading;
-//}
-
-int fCO() {
-  return analogRead(pCOS);
-}
-
-float hum() { 
-  byte address, Hum_H, Hum_L, Temp_H, Temp_L, _status;
-  unsigned int H_dat, T_dat;
-  float RH;
-  
-  address = 0x27;
-  Wire.beginTransmission(address); 
-  Wire.endTransmission();
-  delay(100);
-  
-  Wire.requestFrom((int)address, (int) 4);
-  Hum_H = Wire.read();
-  Hum_L = Wire.read();
-  Temp_H = Wire.read();
-  Temp_L = Wire.read();
-  
-  Wire.endTransmission();
-  
-  _status = (Hum_H >> 6) & 0x03;
-  
-  Hum_H = Hum_H & 0x3f;
-  H_dat = (((unsigned int)Hum_H) << 8) | Hum_L;
-  T_dat = (((unsigned int)Temp_H) << 8) | Temp_L;
-  T_dat = T_dat / 4;
-  
-  RH = H_dat * 6.10e-3;   
-  
-  return(RH);
-}
-
-float temp() { 
-  byte address, Hum_H, Hum_L, Temp_H, Temp_L, _status;
-  unsigned int H_dat, T_dat;
-  float T_C;
-  
-  address = 0x27;
-  Wire.beginTransmission(address); 
-  Wire.endTransmission();
-  delay(100);
-  
-  Wire.requestFrom((int)address, (int) 4);
-  Hum_H = Wire.read();
-  Hum_L = Wire.read();
-  Temp_H = Wire.read();
-  Temp_L = Wire.read();
-  
-  Wire.endTransmission();
-  
-  _status = (Hum_H >> 6) & 0x03;
-  
-  Hum_H = Hum_H & 0x3f;
-  H_dat = (((unsigned int)Hum_H) << 8) | Hum_L;
-  T_dat = (((unsigned int)Temp_H) << 8) | Temp_L;
-  T_dat = T_dat / 4;
-  
-  T_C = T_dat * 1.007e-2 - 40.0;   
-  
-  return(T_C);
-}  
 
 float getMotion(int MSB1, int LSB1){
   int motion_analog = MSB1 * 256 + LSB1;
