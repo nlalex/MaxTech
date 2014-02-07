d82a85ae3b9fcd4ebbb98ee17c9d393a0ba761d1
nlalex <nathan.l.alexander@gmail.com>
2014-02-06 20:27:53 -0500

    Create Mesh.h

---
+++ Mesh/Mesh.h
@@ -0,0 +1,24 @@
+#ifndef Mesh_h
+#define Mesh_h
+
+#include <WProgram.h>
+#include <XBee.h>
+#include "Config.h"
+
+class Mesh {
+  public:
+    Node(char address[]);
+    ~Node();
+    void stash();
+    void flush();
+    float convertTemp(int temp);
+    float convertHum(int hum);
+  private:
+    char _address[];
+    float temp[];
+    float hum[];
+    int light1[];
+    int light2[];
+};
+
+#endif
