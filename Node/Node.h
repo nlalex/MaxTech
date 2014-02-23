45795f2ead3906e62fcd0a4cb532a8c74408663f
nlalex <nathan.l.alexander@gmail.com>
2014-02-22 21:38:14 -0500

    Changed convert* type & removed inputs

--- Node/Node.h
+++ Node/Node.h
@@ -11,9 +11,9 @@ class Node {
     ~Node(); //class destructor -> does nothing
     void stash(ZBRxIoSampleResponse packet); //stores packet data to class variables
     void flush(); //resets all class variables to 0
-    float convertTemp(int temp); //converts analog value to temperature in degF
-    float convertHum(int hum);
-    byte convertMotion(byte pir, byte motion); //allows any motion within reporting window to be recongnized
+    void convertTemp(); //converts analog value to temperature in degF
+    void convertHum();
+    void convertMotion(); //allows any motion within reporting window to be recongnized
     boolean matchAddr(ZBRxIoSampleResponse packet); //checks for address match
     void printAll(); //prints all variables to console via Serial for testing purposes
     XBeeAdress64 addr; //node address placeholder
