88c0125757b07eba9dd5ea7b458933c0f8743c37
nlalex <nathan.l.alexander@gmail.com>
2014-02-23 14:08:18 -0500

    Private variables; constructor input types

--- Node/Node.h
+++ Node/Node.h
@@ -7,7 +7,7 @@
 
 class Node {
   public:
-    Node(str msb_in, str lsb_in, str loc_in); //class constructor -> initializes variables
+    Node(unit32_t msb_in, unit32_t lsb_in, String loc_in); //class constructor -> initializes variables
     ~Node(); //class destructor -> does nothing
     void stash(ZBRxIoSampleResponse packet); //stores packet data to class variables
     void flush(); //resets all class variables to 0
@@ -17,13 +17,14 @@ class Node {
     boolean matchAddr(ZBRxIoSampleResponse packet); //checks for address match
     void printAll(); //prints all variables to console via Serial for testing purposes
     XBeeAdress64 addr; //node address placeholder
-    float temp;
-    float hum;
-    int ldr1;
-    int ldr2;
-    byte motion;
+    String loc;
   private:
+    float _temp;
+    float _hum;
+    int _ldr1;
+    int _ldr2;
     byte _pir; //temporary motion holder for raw data
+    byte _motion;
 };
 
 #endif
