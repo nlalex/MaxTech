1dd3dd1442045c5549e48328092e116b283780c5
nlalex <nathan.l.alexander@gmail.com>
2014-02-22 20:59:34 -0500

    Update stash(), checkPIR(), covertMotion()

--- Node/Node.cpp
+++ Node/Node.cpp
@@ -1,7 +1,7 @@
 #include "Node.h"
-#include <WProgram.h>
-#include <XBee.h>
-#include "Config.h"
+//#include <WProgram.h>
+//#include <XBee.h>
+//#include "Config.h"
 
 Node::Node(str msb_in, str lsb_in, str loc_in) {
   addr = XBeeAdress64(msb_in, lsb_in);
@@ -10,13 +10,17 @@ Node::Node(str msb_in, str lsb_in, str loc_in) {
   hum = 0;
   ldr1 = 0;
   ldr2 = 0;
-  PIR = false;
+  motion = false;
 }
 
 Node::~Node() {}
 
-void Node::stash() {
-  
+void Node::stash(ZBRxIoSampleResponse packet) {
+  temp = packet.getAnalog(pTEMP);
+  hum = packet.getAnalog(pHUM);
+  ldr1 = packet.getAnalog(pLDR1);
+  ldr2 = packet.getAnalog(pLDR2);
+  _pir = packet.getDigital(pPIR);
 }
 
 void Node::flush() {
@@ -24,7 +28,8 @@ void Node::flush() {
   hum = 0;
   ldr1 = 0;
   ldr2 = 0;
-  PIR = false;
+  _pir = 0;
+  motion = 0;
 }
 
 float Node::convertTemp(int temp) {
@@ -39,6 +44,10 @@ float Node::convertHum(int hum) {
   return float(hum);
 }
 
-bool Node::checkPIR(int PIR) {
-  return bool(PIR);
+byte Node::convertMotion(byte pir, byte motion) {
+  if(byte pir == 0 && motion == 0) {
+    return 0;
+  } else {
+    return 1;
+  }
 }
