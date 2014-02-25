9e11347375c136466027543c085263891c0d5d7a
nlalex <nathan.l.alexander@gmail.com>
2014-02-24 20:52:20 -0500

    Added hub pins

--- Node/Config.h
+++ Node/Config.h
@@ -1,5 +1,13 @@
-const int pTEMP = 2;
-const int pHUM = 1;
 const int pLDR1 = 0;
+const int pHUM = 1;
+const int pTEMP = 2;
 const int pLDR2 = 3;
 const int pPIR = 4;
+
+
+const int pTEMPh = 0;
+const int pHUMh = 1;
+const int pLDR1h = 2;
+const int pLDR2 = 3;
+
+const int pPIRh = 0;
