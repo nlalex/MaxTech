033e5a109bfd756f8712a25f21438e9887d64b9b
nlalex <nathan.l.alexander@gmail.com>
2014-03-18 17:18:44 -0400

    Changed to recursive layout

--- TestNode01/TestNode01.ino
+++ TestNode01/TestNode01.ino
@@ -15,6 +15,9 @@ Node node4 = Node(XBeeAddress64(0x0013A200,0x40AEBA2C), 4);
 Node node5 = Node(XBeeAddress64(0x0013A200,0x40AEB9AA), 5);
 Node node6 = Node(XBeeAddress64(0x0013A200,0x40AEB9C3), 6);
 
+Node nodes[] = {node2, node3, node4, node5, node6};
+int nodeCount = 5;
+
 unsigned long last_time;
 
 void setup()
@@ -53,32 +56,41 @@ void loop() {
     if (xbee.getResponse().getApiId() == ZB_IO_SAMPLE_RESPONSE) {
       xbee.getResponse().getZBRxIoSampleResponse(response);
       
-      if(node2.matchAddress(response)) {
-        node2.stashConvert(response);
-        node2.printAll();
-        //node2.testDatabaseSend();
-        node2.flush();
-      } else if(node3.matchAddress(response)) {
-        node3.stashConvert(response);
-        node3.printAll();
-        //node3.testDatabaseSend();
-        node3.flush();
-      } else if(node4.matchAddress(response)) {
-        node4.stashConvert(response);
-        node4.printAll();
-        //node4.testDatabaseSend();
-        node4.flush();
-      } else if(node5.matchAddress(response)) {
-        node5.stashConvert(response);
-        node5.printAll();
-        //node5.testDatabaseSend();
-        node5.flush();
-      } else if(node6.matchAddress(response)) {
-        node6.stashConvert(response);
-        node6.printAll();
-        //node6.testDatabaseSend();
-        node6.flush();
+      for(int i=0; i < nodeCount; i++) {
+        if(nodes[i].matchAddress(response)) {
+          nodes[i].stashConvert(response);
+          nodes[i].printAll();
+          nodes[i].testDatabaseSend();
+          nodes[i].flush();
+        }
       }
+      
+//      if(node2.matchAddress(response)) {
+//        node2.stashConvert(response);
+//        node2.printAll();
+//        //node2.testDatabaseSend();
+//        node2.flush();
+//      } else if(node3.matchAddress(response)) {
+//        node3.stashConvert(response);
+//        node3.printAll();
+//        //node3.testDatabaseSend();
+//        node3.flush();
+//      } else if(node4.matchAddress(response)) {
+//        node4.stashConvert(response);
+//        node4.printAll();
+//        //node4.testDatabaseSend();
+//        node4.flush();
+//      } else if(node5.matchAddress(response)) {
+//        node5.stashConvert(response);
+//        node5.printAll();
+//        //node5.testDatabaseSend();
+//        node5.flush();
+//      } else if(node6.matchAddress(response)) {
+//        node6.stashConvert(response);
+//        node6.printAll();
+//        //node6.testDatabaseSend();
+//        node6.flush();
+//      }
     }
   }
 }
