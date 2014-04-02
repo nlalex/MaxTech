7c41fbb38baf44881dfb1f7dca84a9fe44a45f83
nlalex <nathan.l.alexander@gmail.com>
2014-04-01 21:55:26 -0400

    Added comments

--- TestHeaters/TestHeaters.ino
+++ TestHeaters/TestHeaters.ino
@@ -1,11 +1,11 @@
-int pHeaters[] = {22, 23, 24, 25, 26, 27};
-int numHeaters = 6;
+int pHeaters[] = {22, 23, 24, 25, 26, 27}; //heater control pins
+int numHeaters = 6; //number of entries in above array
 
-const unsigned long tSleep = 5000;
+const unsigned long tSleep = 5000; //amount of time between switching
 
 void setup() {
   Serial.begin(9600);
-  for(int i=0; i<numHeaters; i++) {
+  for(int i=0; i<numHeaters; i++) { //set all pins as output
       pinMode(pHeaters[i], OUTPUT);
       Serial.print("Set ");
       Serial.println(i);
@@ -13,14 +13,17 @@ void setup() {
 }
 
 void loop() {
-  for(int i=0; i<numHeaters; i++) {
+  for(int i=0; i<numHeaters; i++) { //set all pins low
       digitalWrite(pHeaters[i], LOW);
       Serial.println("Off");
   }
+  
   delay(tSleep);
-  for(int i=0; i<numHeaters; i++) {
+  
+  for(int i=0; i<numHeaters; i++) { //set all pins high
       digitalWrite(pHeaters[i], HIGH);
       Serial.println("On");
   }
+  
   delay(tSleep);
 }
