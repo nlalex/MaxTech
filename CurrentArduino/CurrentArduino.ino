54ff7402143bea28a8c214992f50c702b6757d83
nlalex <nathan.l.alexander@gmail.com>
2014-04-02 16:53:46 -0400

    Cleaned code

--- CurrentArduino/CurrentArduino.ino
+++ CurrentArduino/CurrentArduino.ino
@@ -28,7 +28,8 @@ Node node3 = Node(addr3, 3);
 Node node4 = Node(addr4, 4);
 Node node5 = Node(addr5, 5);
 Node node6 = Node(addr6, 6);
-Node nodes[] = {hub, node2, node3, node4, node5, node6}; //Array containing previously defined Nodes
+Node nodes[] = {
+  hub, node2, node3, node4, node5, node6}; //Array containing previously defined Nodes
 //Node nodes[] = {hub, node2, node3, node4, node5, node6};
 int nodeCount = 6;
 
@@ -48,10 +49,10 @@ void setup()
 {
   //analogReference(EXTERNAL);
   if(DEBUG) Serial.begin(9600); //For communication to/from computer
- 
+
   Serial1.begin(9600); //For communication to/from XBee with Mega
   xbee.setSerial(Serial1);
-  
+
 
   if(DEBUG) Serial.println("Initializing & closing all vents");
   for(int i=0; i<nodeCount; i++) { //may need dummy pins defined for padding
@@ -59,86 +60,58 @@ void setup()
     pinMode(pVentNeg[i], OUTPUT);
     pinMode(pVentEnable[i], OUTPUT);
   }
-  //heatersOFF();
-//  ventsOpen();
-//  delay(1000);
-//  ventsClose();
 
-  
   if(digitalRead(pCAL) == HIGH) {
     setEqual();
   }
-  
+
   // attempt to connect to Wifi network:
   connectWifi();
   if(DEBUG) printWifiStatus();
 
   last_time = millis();
 }
- 
+
 
 void loop() {
   if(millis()-last_time <= send_time) { //Timed loop functions
     xbee.readPacket();
     if (xbee.getResponse().isAvailable()) {
       if (xbee.getResponse().getApiId() == ZB_IO_SAMPLE_RESPONSE) {
-        //xbee.getResponse().setApiId(ZB_IO_SAMPLE_RESPONSE);
-        //Serial.println(xbee.getResponse().getApiId());
-        //xbee.getResponse().getZBRxIoSampleResponse(response);
-        //response.setApiId(ZB_IO_SAMPLE_RESPONSE);
         xbee.getResponse().getZBRxIoSampleResponse(response);
         for(int i=0; i < nodeCount; i++) {
           if(nodes[i].matchAddress(response)) {
             nodes[i].stashConvert(response);
-            nodes[i].printAllCompact();
+            if(DEBUG) nodes[i].printAllCompact();
             if(DEBUG) {
               nodes[0].stashConvertHub();
               nodes[0].printAllCompact();
             }
           }
         }
-        
-
-        xbee.getResponse().getZBRxIoSampleResponse(response);
-        for(int i=0; i < nodeCount; i++) {
-          if(nodes[i].matchAddress(response)) {
-              nodes[i].stashConvert(response);
-              if(DEBUG) nodes[i].printAllCompact();
-            }
+      } 
+      else {
+        if(DEBUG) {
+          Serial.print("Packet API ID not recognized: ");
+          Serial.println(xbee.getResponse().getApiId(), HEX);
         }
-
       }
     }
   } else {  
     nodes[0].stashConvertHub();
-    
-//    float sumTemp = 0.0;
-//    for(int i=1; i<nodeCount; i++) {
-//      sumTemp += nodes[i].temp;
-//    }
-//    float referenceTemp = sumTemp/5;
-//    float referenceTemp = nodes[0].temp;
-//    control1(referenceTemp);
-//    if(DEBUG) {
-//      Serial.print("Reference temperature: ");
-//      Serial.println(referenceTemp);
-//    }
-    
-//    getSettings();
+
     unsigned long start_send = millis();
     if(DEBUG) Serial.println("Sending data...");
     for(int i=0; i < nodeCount; i++) {
       if(DEBUG) nodes[i].printAllCompact();
       sendData(i);
-    }
-      
-      
+    }     
     if(DEBUG) {
       Serial.print("All data sent in ");
       Serial.print((millis()-start_send)/1000.);
       Serial.println(" seconds");
     }
-//    
+
     last_time = millis();
   }
 }
@@ -150,11 +123,11 @@ void setEqual() {
   if(DEBUG){
     Serial.println("Beginning calibaration process...");
   }
-  
+
   int tripCount = 0;
-  
+
   last_time = millis();
-  
+
   while(tripCount != nodeCount && millis() - last_time <= wait_time) { //wait until all nodes are accounted for or times out
     tripCount = 0;
     xbee.readPacket();
@@ -175,28 +148,28 @@ void setEqual() {
       }
     }
   }
-  
-  
-  
+
+
+
   if(DEBUG) {
     Serial.print(tripCount);
     Serial.print(" of ");
     Serial.print(nodeCount);
     Serial.println(" nodes accounted for.");
   }
-  
+
   if(tripCount == nodeCount) {
     hub.stashConvertHub();
-  
+
     if(DEBUG) {
       Serial.println("Calibration completed.");
       Serial.println("Reference values (node, temperature adjustment, humidity adjustment)");
     }
-  
+
     for(int i=0; i < nodeCount; i++) {
       nodes[i].tAdjust = hub.temp - nodes[i].temp;
       nodes[i].hAdjust = hub.hum - nodes[i].hum;
-    
+
       if(DEBUG) {
         Serial.print("Node ");
         Serial.print(i+2);
@@ -206,7 +179,8 @@ void setEqual() {
         Serial.println(nodes[i].hAdjust);
       }
     }
-  } else { //if not all nodes accounted for
+  } 
+  else { //if not all nodes accounted for
     if(DEBUG) Serial.println("Calibration unsuccessful");
   }
 }
@@ -214,57 +188,63 @@ void setEqual() {
 
 void control1(float referenceTemp) { //control scheme using single reference temperature (hub temp)
   if(DEBUG)  Serial.println("Beginning deadband control checks with single reference...");
-  
- // hub.stashConvertHub();
-  
+
+  // hub.stashConvertHub();
+
   if(nodes[0].trip) {
     if(referenceTemp > highTemp) {
       //actuate to lower all temps
       if(DEBUG) Serial.println("Temperatures need lowered.");
-      
+
       //heatersOFF();
       ventsOpen();
-      
-    } else if(referenceTemp < lowTemp) {
+
+    } 
+    else if(referenceTemp < lowTemp) {
       //actuate to raise temp
       if(DEBUG) Serial.println("Temperatures need raised.");
-      
+
       //heatersON();
       ventsClose();
-      
-    } else {
+
+    } 
+    else {
       //do nothing
       if(DEBUG) Serial.println("Temperatures are good.");
     }
-  } else {
+  } 
+  else {
     //data not received from hub
     if(DEBUG) Serial.println("Hub contains null data.");
   }
-  
+
   if(DEBUG) Serial.println("Control checks completed.");
 }
 
 
 void control2() { //control scheme using individual node temperatures
   if(DEBUG)  Serial.println("Beginning deadband control checks with individual references...");
-  
+
   if(hub.trip) {
     if(hub.temp > highTemp) {
       //actuate to lower temp
       if(DEBUG) Serial.println("Hub temperature needs lowered.");
-    } else if(hub.temp < lowTemp) {
+    } 
+    else if(hub.temp < lowTemp) {
       //actuate to raise temp
       if(DEBUG) Serial.println("Hub temperature needs raised.");
-    } else {
+    } 
+    else {
       //do nothing
       if(DEBUG) Serial.println("Hub temperature is good.");
     }
-  } else {
+  } 
+  else {
     //data not received from hub
     if(DEBUG) Serial.println("Hub contains null data.");
   }
-    
-  
+
+
   for(int i=0; i < nodeCount; i++) {
     if(DEBUG) {
       Serial.print("Node ");
@@ -274,19 +254,22 @@ void control2() { //control scheme using individual node temperatures
       if(nodes[i].temp > highTemp) {
         //actuate to lower temp
         if(DEBUG)  Serial.println(" temperature needs lowered.");
-      } else if(nodes[i].temp < lowTemp) {
+      } 
+      else if(nodes[i].temp < lowTemp) {
         //actuate to raise temp
         if(DEBUG) Serial.println(" temperature needs raised.");
-      } else {
+      } 
+      else {
         //do nothing
         if(DEBUG) Serial.println(" temperature is good.");
       }
-    } else {
+    } 
+    else {
       //data not received from node
       if(DEBUG) Serial.println(" contains null data.");
     }
   }
-  
+
   if(DEBUG) {
     Serial.println("Control checks completed.");
     Serial.println("");
@@ -302,8 +285,8 @@ void printCSV() {
 
 void heatersOFF() {
   for(int i=0; i < nodeCount; i++) {
-//    delay(10);
-//    digitalWrite(pHeaters[i], HIGH); //high turns heaters off
+    //    delay(10);
+    //    digitalWrite(pHeaters[i], HIGH); //high turns heaters off
     nodes[i].actuatedOFF();
   }
   for(int i=0; i < nodeCount+2; i++) {
@@ -314,8 +297,8 @@ void heatersOFF() {
 
 void heatersON() {
   for(int i=0; i < nodeCount; i++) {
-//    delay(10);
-//    digitalWrite(pHeaters[i], LOW); //low turns heaters on
+    //    delay(10);
+    //    digitalWrite(pHeaters[i], LOW); //low turns heaters on
     nodes[i].actuatedON();
   }
   for(int i=0; i < nodeCount+2; i++) {
@@ -386,18 +369,18 @@ void ventClose(int p) {
 
 void ventsClose() {
   if(vOpen == true) {
-  for(int i=0; i<nodeCount; i++) {
-    ventClose(i);
-  }
+    for(int i=0; i<nodeCount; i++) {
+      ventClose(i);
+    }
   }
   vOpen = false;
 }
 
 void ventsOpen() {
   if(vOpen == false) {
-  for(int i=0; i<nodeCount; i++) {
-    ventOpen(i);
-  }
+    for(int i=0; i<nodeCount; i++) {
+      ventOpen(i);
+    }
   }
   vOpen = true;
 }
@@ -408,56 +391,61 @@ void sendData(int i) {
   client.stop();
   if(client.connect(server, 80)) {
     if(nodes[i].trip) {
-        while(!client.connected()) { // && millis()-tStart<tSendTimeout
-			client.stop();
-			if(DEBUG) Serial.println("Problems connecting.  Trying again...");
-			client.flush();
-			client.connect(server, 80);
-			delay(1000);
-		}
+      while(!client.connected()) { // && millis()-tStart<tSendTimeout
+        client.stop();
+        if(DEBUG) Serial.println("Problems connecting.  Trying again...");
+        client.flush();
+        client.connect(server, 80);
+        delay(1000);
+      }
 
-            client.print("GET /hook2.php?node=");
-            client.print(nodes[i].num);
-            client.print("&temp=");
-            client.print(nodes[i].temp);
-            client.print("&humidity=");
-            client.print(nodes[i].hum);
-            client.print("&light1=");
-            client.print(nodes[i]._ldr1);
-            client.print("&light2=");
-            client.print(nodes[i]._ldr2);
-            client.print("&motion=");
-            client.print(nodes[i]._pir);
-            client.print("&heat=");
-            client.print(nodes[i].actuated);
-            client.print("&crt=");
-            client.print(nodes[i].ct);
-            client.println(" HTTP/1.1");
-		client.println("Host: mesh.org.ohio-state.edu");
-		client.println("User-Agent: ArduinoWiFi/1.1");
-		client.println("Connection: close");
-		client.println();
-            //Serial.print("Data send to node ");
-            //Serial.println(i+1);
-            sendCheck = 0;
-            delay(3000);
-      } else sendCheck = 1;
-    } else {
-            //Serial.println("connection failed");
-            client.stop();
-            sendCheck = 2;
-    }  
+      client.print("GET /hook2.php?node=");
+      client.print(nodes[i].num);
+      client.print("&temp=");
+      client.print(nodes[i].temp);
+      client.print("&humidity=");
+      client.print(nodes[i].hum);
+      client.print("&light1=");
+      client.print(nodes[i]._ldr1);
+      client.print("&light2=");
+      client.print(nodes[i]._ldr2);
+      client.print("&motion=");
+      client.print(nodes[i]._pir);
+      client.print("&heat=");
+      client.print(nodes[i].actuated);
+      client.print("&crt=");
+      client.print(nodes[i].ct);
+      client.println(" HTTP/1.1");
+      client.println("Host: mesh.org.ohio-state.edu");
+      client.println("User-Agent: ArduinoWiFi/1.1");
+      client.println("Connection: close");
+      client.println();
+      //Serial.print("Data send to node ");
+      //Serial.println(i+1);
+      sendCheck = 0;
+      delay(3000);
+    } 
+    else sendCheck = 1;
+  } 
+  else {
+    //Serial.println("connection failed");
+    client.stop();
+    sendCheck = 2;
+  }  
   if(DEBUG) {
     if(sendCheck == 0) {
       Serial.print(nodes[i].num);
       Serial.println(" sent successfully");
-    } else if(sendCheck == 1) {
+    } 
+    else if(sendCheck == 1) {
       Serial.print(nodes[i].num);
       Serial.println(" did not send -> contains null data");
-    } else if(sendCheck == 2) {
+    } 
+    else if(sendCheck == 2) {
       Serial.print(nodes[i].num);
       Serial.println(" did not send -> could not connect");
-    } else {
+    } 
+    else {
       Serial.print(nodes[i].num);
       Serial.println(" had unknown error");
     }
@@ -473,7 +461,7 @@ void getSettings() {
   int response_end = 0;
   char c[] = "";
   char buffer[10];
-  
+
   if (client.connect(server, 80)) {
     Serial.println("connecting...");
     // send the HTTP PUT request:
@@ -490,20 +478,20 @@ void getSettings() {
   }
   response_start = http_response.indexOf("<data>")+6; 
   response_end = http_response.indexOf("</data>");
-  
+
   char httpParse[response_end-response_start];
   for(int i=0; i<(response_end-response_start); i++){
-     httpParse[i] = http_response.charAt(i+response_start);
+    httpParse[i] = http_response.charAt(i+response_start);
   }
- 
+
   for(int i=0; i<6; i++){
-     settings_high[i] = (httpParse[3+9*i]-48)*10+(httpParse[4+9*i]-48); 
+    settings_high[i] = (httpParse[3+9*i]-48)*10+(httpParse[4+9*i]-48); 
   }    
-  
+
   for(int i=0; i<6; i++){
-     settings_low[i] = (httpParse[6+9*i]-48)*10+(httpParse[7+9*i]-48); 
+    settings_low[i] = (httpParse[6+9*i]-48)*10+(httpParse[7+9*i]-48); 
   }  
-  
+
   if(DEBUG) {
     Serial.println("Low/high temp settings: ");
     for(int i=0; i<nodeCount; i++) {
@@ -513,7 +501,8 @@ void getSettings() {
     }
   }
 }
-      
-      
+
+
+
 
 
--- libraries/Node/Node.cpp
+++ libraries/Node/Node.cpp
@@ -1,8 +1,5 @@
 #include "Node.h"
 #include <WProgram.h>
-// #include <XBee.h>
-// #include <WiFi.h>
-// #include "Config_enCORE.h"
 
 Node::Node(XBeeAddress64 addr_in, int num_in) {
   addr = addr_in;
