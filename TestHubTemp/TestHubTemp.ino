89529ea5f96e30f61034aba6fd800d9c460708cc
nlalex <nathan.l.alexander@gmail.com>
2014-04-01 21:53:26 -0400

    Added comments

--- TestHubTemp/TestHubTemp.ino
+++ TestHubTemp/TestHubTemp.ino
@@ -1,12 +1,14 @@
+const int pTemp = 12; //define analog pin here
+
 void setup() {
   Serial.begin(9600);
-  analogReference(EXTERNAL);
+  analogReference(EXTERNAL); //only enable if using reference value other than 5V
 }
 
 void loop() {
-  int temp_analog = analogRead(12);
+  int temp_analog = analogRead(pTemp);
   float voltage = temp_analog * 5000.;
-  voltage /= 1024.0;  
+  voltage /= 1024.0;
   float temperatureC = (voltage - 500.0)/ 10.0 ;
   float temperatureF = ((temperatureC * 9.0) / 5.0) + 32.0;
   float temp = temperatureF;
