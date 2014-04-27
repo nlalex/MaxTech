3f2767c4a7204d962940a00490e30922825e3558
nlalex <nathan.l.alexander@gmail.com>
2014-04-27 16:29:16 -0400

    Updated indenting

--- libraries/Node/Node.cpp
+++ libraries/Node/Node.cpp
@@ -12,7 +12,7 @@ Node::Node(XBeeAddress64 addr_in, int num_in) {
   tAdjust = 0;
   hAdjust = 0;
   trip = false;
- // _motion = 0;
+  // _motion = 0;
   if(num == HUB_NUM) {
     pinMode(pPIRh, INPUT);
   }
@@ -21,7 +21,8 @@ Node::Node(XBeeAddress64 addr_in, int num_in) {
   active = 0;
 }
 
-Node::~Node() {}
+Node::~Node() {
+}
 
 void Node::stash(ZBRxIoSampleResponse packet) {
   temp = packet.getAnalog(pTEMP);
@@ -29,19 +30,19 @@ void Node::stash(ZBRxIoSampleResponse packet) {
   _ldr1 = packet.getAnalog(pLDR1);
   _ldr2 = packet.getAnalog(pLDR2);
   switch (packet.isDigitalOn(pPIR)) {
-    case 0:
-      _pir = 1;
-      break;
-    case 1:
-      _pir = 0;
-      break;
+  case 0:
+    _pir = 1;
+    break;
+  case 1:
+    _pir = 0;
+    break;
   }
   trip = true;
 
 }
 
 void Node::stashHub() {
-	Serial.println("Reading hub");
+  Serial.println("Reading hub");
   _ldr1 = analogRead(pLDR1h);
   delay(10);
   _ldr1 = analogRead(pLDR1h);
@@ -59,19 +60,20 @@ void Node::stashHub() {
   hum = analogRead(pHUMh);
   delay(10);
   switch (digitalRead(pPIRh)) {
-    case 0:
-      _pir = 1;
-      break;
-    case 1:
-      _pir = 0;
-      break;
+  case 0:
+    _pir = 1;
+    break;
+  case 1:
+    _pir = 0;
+    break;
   }
   trip = true;
   if(analogRead(pCT)>threshCT) {
-	 ct = 1;
-   }else {
-	 ct = 0;
-	}
+    ct = 1;
+  }
+  else {
+    ct = 0;
+  }
 }
 
 void Node::flush() {
@@ -80,8 +82,8 @@ void Node::flush() {
   _ldr1 = 0;
   _ldr2 = 0;
   _pir = 0;
- // _motion = 0;
- trip = false;
+  // _motion = 0;
+  trip = false;
 }
 
 void Node::convertTemp() {
@@ -137,7 +139,8 @@ void Node::convertHumHub() {
 boolean Node::matchAddress(ZBRxIoSampleResponse packet) {
   if(packet.getRemoteAddress64().getLsb()==addr.getLsb() && packet.getRemoteAddress64().getMsb()==addr.getMsb()) {
     return true;
-  } else {
+  } 
+  else {
     return false;
   }
 }
@@ -149,25 +152,25 @@ void Node::printAll() {
   Serial.print("Node Address: ");
   Serial.print(addr.getMsb(), HEX);
   Serial.println(addr.getLsb(), HEX);
-  
+
   Serial.print("Temperature: ");
   Serial.println(temp);
-  
+
   Serial.print("Humidity: ");
   Serial.println(hum);
-  
+
   Serial.print("Light #1: ");
   Serial.println(_ldr1);
-  
+
   Serial.print("Light #2: ");
   Serial.println(_ldr2);
-  
+
   Serial.print("Motion: ");
   Serial.println(_pir);
-  
+
   Serial.print("Actuated: ");
   Serial.println(actuated);
-  
+
   Serial.println("");
 }
 
@@ -220,56 +223,34 @@ void Node::testDatabaseSend() {
 }
 
 int Node::sendToDatabase(WiFiClient client) {
-	client.flush();
-	client.stop();
-	if(client.connect(server, 80)) {
-	//client.connect(server,80);
-	  //if(trip) {
-		//long tStart = millis();
-		// while(!client.connected()) { // && millis()-tStart<tSendTimeout
-			// client.stop();
-			// client.flush();
-			// client.connect(server, 80);
-			// delay(100);
-		// }
-		// client.print("GET /hook1.php?node=");
-		// client.print(num);
-		// client.print("&temp=");
-		// client.print(temp);
-		// client.print("&humidity=");
-		// client.print(hum);
-		// client.print("&light1=");
-		// client.print(_ldr1);
-		// client.print("&light2=");
-		// client.print(_ldr2);
-		// client.print("&motion=");
-		// client.print(_pir);
-		// client.print("&heat=");
-		// client.print(actuated);
-		// client.println(" HTTP/1.1");
-		client.println("GET /hook1.php?node=1&temp=70&humidity=20.5&light1=500&light2=400&motion=1&heat=1 HTTP/1.1");
-		client.println("Host: mesh.org.ohio-state.edu");
-		client.println("User-Agent: ArduinoWiFi/1.1");
-		client.println("Connection: close");
-		client.println();
-		
-		delay(tWaitSend);
-		// client.stop();
-		return 0;
-	   //} else {
-			//client.stop();
-		//	return 1;
-		//}
-	} else {
-		client.stop();
-		return 2;
-	}
+  client.flush();
+  client.stop();
+  if(client.connect(server, 80)) {
+    client.println("GET /hook1.php?node=1&temp=70&humidity=20.5&light1=500&light2=400&motion=1&heat=1 HTTP/1.1");
+    client.println("Host: mesh.org.ohio-state.edu");
+    client.println("User-Agent: ArduinoWiFi/1.1");
+    client.println("Connection: close");
+    client.println();
+
+    delay(tWaitSend);
+    // client.stop();
+    return 0;
+    //} else {
+    //client.stop();
+    //	return 1;
+    //}
+  } 
+  else {
+    client.stop();
+    return 2;
+  }
 }
 
 void Node::isActuated(int bActuated) {
-	actuated = bActuated;
+  actuated = bActuated;
 }
 
 void Node::isActive(int bActive) {
-	active = bActive;
+  active = bActive;
 }
+
